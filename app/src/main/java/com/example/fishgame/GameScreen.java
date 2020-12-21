44ed1ee5b05d16f540143d781667657e03a26e86
Anton Badretdinov <antonbad03@gmail.com>
2020-12-21 15:24:23 +0700

    add borders and remove navigation bar

--- app/src/main/java/com/example/fishgame/GameScreen.java
+++ app/src/main/java/com/example/fishgame/GameScreen.java
@@ -7,6 +7,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -35,10 +36,20 @@ public class GameScreen extends View {
         }
     }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        screenHeight = h;
+        screenWidth = w;
+    }
+
     private float fishX=0;
     private float fishY=0;
     private float goalX=0;
     private float goalY=0;
+    private float screenWidth;
+    private float screenHeight;
 
 
     @Override
@@ -49,6 +60,7 @@ public class GameScreen extends View {
         Bitmap fish = BitmapFactory.decodeResource(getResources(),R.drawable.fish);
         fish = Bitmap.createScaledBitmap(fish,200,200,true);
         canvas.drawBitmap(fish,fishX,fishY,new Paint());
+
         if(fishX<goalX){
             fishX+=10;
         }else if(fishX>goalX){
@@ -68,14 +80,14 @@ public class GameScreen extends View {
         }
 
 
-        if(fishX>1080){
-            fishX=1080;
+        if(fishX>screenWidth-150){
+            fishX=screenWidth-150;
         }else if(fishX<0){
             fishX=0;
         }
 
-        if(fishY>1550){
-            fishY=1550;
+        if(fishY>screenHeight-150){
+            fishY=screenHeight-150;
         }else if(fishY<0){
             fishY=0;
         }
@@ -86,6 +98,7 @@ public class GameScreen extends View {
         if(event.getAction()==MotionEvent.ACTION_DOWN){
             goalX = event.getX();
             goalY = event.getY();
+            Log.d(Float.toString(goalX),Float.toString(goalY));
         }
 
         return super.onTouchEvent(event);
--- app/src/main/java/com/example/fishgame/MainActivity.java
+++ app/src/main/java/com/example/fishgame/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fishgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new GameScreen(this));
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
